d45fe2bcb1bf00e057ca468d46d9eb255d21025c
Alekberovulvi <ulvina@code.edu.az>
2023-08-29 22:51:56 +0400

    TasksAdas

---
+++ Mentor Task/Task23.08.29/assets/js/script.js
@@ -0,0 +1,163 @@
+//first
+
+// function hello() {
+//     return "hello world";
+// }
+
+// console.log(hello());  
+
+
+//seconds
+
+// function addition(a, b) {
+//     return a + b;
+// }
+
+// console.log(addition(3, 2));   
+// console.log(addition(-3, -6)); 
+// console.log(addition(7, 3));   
+
+
+ //third
+
+// function convert(minutes) {
+//     return minutes * 60;
+// }
+
+// console.log(convert(5)); 
+// console.log(convert(3)); 
+// console.log(convert(2)); 
+
+// fourth
+
+// function addition(number) {
+//     return number + 1;
+// }
+
+// console.log(addition(0));  
+// console.log(addition(9));  
+// console.log(addition(-3)); 
+
+
+//fifth
+
+// function triArea(base, height) {
+//     return (base * height) / 2;
+// }
+
+// console.log(triArea(3, 2));   
+// console.log(triArea(7, 4));   
+// console.log(triArea(10, 10)); 
+
+//sixth
+
+// function calcAge(ageInYears) {
+//     return ageInYears * 365;
+// }
+
+// console.log(calcAge(65)); 
+// console.log(calcAge(0));  
+// console.log(calcAge(20)); 
+
+//seventh
+
+// function cubes(a) {
+//     return a ** 3;
+// }
+
+// console.log(cubes(3));  
+// console.log(cubes(5));  
+// console.log(cubes(10)); 
+
+
+//eighth
+
+// function getFirstValue(arr) {
+//     return arr[0];
+// }
+
+// console.log(getFirstValue([1, 2, 3]));     
+// console.log(getFirstValue([80, 5, 100])); 
+// console.log(getFirstValue([-500, 0, 50])); 
+
+
+//ninth
+
+// function circuitPower(voltage, current) {
+//     return voltage * current;
+// }
+
+// console.log(circuitPower(230, 10)); 
+// console.log(circuitPower(110, 3));  
+// console.log(circuitPower(480, 20)); 
+
+
+//tenth
+
+// function howManySeconds(hours) {
+//     return hours * 3600; 
+// }
+
+// console.log(howManySeconds(2));  
+// console.log(howManySeconds(10)); 
+// console.log(howManySeconds(24)); 
+
+
+//eleventh
+
+// function remainder(a, b) {
+//     return a % b;
+// }
+
+// console.log(remainder(1, 3));   
+// console.log(remainder(3, 4));   
+// console.log(remainder(-9, 45)); 
+// console.log(remainder(5, 5));   
+
+
+//twelfth
+
+// function findPerimeter(length, width) {
+//     return 2 * (length + width);
+// }
+
+// console.log(findPerimeter(6, 7));  
+// console.log(findPerimeter(20, 10)); 
+// console.log(findPerimeter(2, 9));   
+
+
+//thirteenth
+
+// function points(twoPointers, threePointers) {
+//     return 2 * twoPointers + 3 * threePointers;
+// }
+
+// console.log(points(1, 1));   
+// console.log(points(7, 5));   
+// console.log(points(38, 8));  
+
+
+//fourteenth
+
+// function lessThan100(a, b) {
+//     return a + b < 100;
+// }
+
+// console.log(lessThan100(22, 15)); 
+// console.log(lessThan100(83, 34)); 
+// console.log(lessThan100(3, 77));  
+
+
+//fifteenth
+
+// function and(a, b) {
+//     return a && b;
+// }
+
+// console.log(and(true, false)); 
+// console.log(and(true, true));  
+// console.log(and(false, true)); 
+// console.log(and(false, false));
+
+
+
