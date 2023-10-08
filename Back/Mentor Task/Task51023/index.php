6fe228d7b351ce214496c9883c4dd115bdc2415d
Alekberovulvi <ulvina@code.edu.az>
2023-10-08 15:34:00 +0400

    51023task

---
+++ Back/Mentor Task/Task51023/index.php
@@ -0,0 +1,78 @@
+<?php 
+
+// 1 
+// $arr = array("Messi","Suarez","Ronaldinho","Neymar","Terstegen","Messi","Suarez","Ronaldinho","Neymar","Terstegen");
+// $reverse = array_reverse($arr);
+// $euro = array_count_values($arr);
+
+// echo end($arr);
+// echo '<br>';
+// echo reset($arr);
+
+// array_push($arr,"Deco");
+// echo '<br>';
+// print_r($arr);
+
+// array_pop($arr);
+// echo '<br>';
+// print_r($arr);
+// echo '<br>';
+// print_r($reverse);
+
+// echo '<br>';
+// print_r($euro);
+
+// echo '<br>';
+// unset($arr[5]);
+// print_r($arr);
+
+// $array1 = array("winston", "sobraine", "baki");
+// $array2 = array("winston", "sobraine", "baki");
+
+// $newArray = array_intersect($array1, $array2);
+
+// print_r($newArray);
+
+
+// unset($arr[4]);
+
+// print_r($arr);
+
+
+// 2
+// $array1 = array(1,2,3,4,5,6,7,8,9,10);
+
+// $max = max($array1);
+
+// $min = min($array1);
+
+// print_r($array1);
+// echo "<br>";
+// echo "en kicik eded " . $min . "<br>";
+// echo "en boyuk eded ". $max;
+
+// echo array_sum($array1);
+
+// echo array_product($array1);
+
+// echo implode(" " , $array1);
+
+
+//3
+
+// $str = "Salam, mən söz verirəm ki, php üzrə mütəxəxssis olacam";
+
+// $newstr = str_replace(' ' , '' , $str);
+
+// echo $newstr;
+
+// $str = strtoupper($str);
+
+// echo $str;
+
+// 4
+
+// $array = ("Hello World" , "<br>" ,  1 , 2, 3, 4 ,5);
+
+// print_r($array);
+
--- Back/Teacher Task/HomeWork03.10.23/index.php
+++ Back/Teacher Task/HomeWork03.10.23/index.php
@@ -52,3 +52,5 @@
 <?php
   require_once('./loyauts/footer.php');
 ?>
+
+
